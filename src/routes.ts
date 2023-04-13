8feffe0641cec46eb814b6b3a8f2d047a61075df
zzzBECK <alexandrebeck2003@gmail.com>
2023-04-13 18:58:19 -0300

    NLW#5 Aula03 0:34:27

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { SettingsController } from "./controller/SettingsController";
+import { UsersController } from "./controller/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 /**
  * tipos de parametros
@@ -13,6 +15,7 @@ const settingsController = new SettingsController();
 */
 
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create)
 
 
 export { routes }
